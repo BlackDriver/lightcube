dc0af672bbf81e4ffc1004c04b34995321c44879
WHY <958272677@qq.com>
2018-10-11 16:12:30 +0800

    仿真和测试代码

---
+++ 888/test/test.ino
@@ -0,0 +1,14 @@
+void setup() {
+  for(int i=19;i<38;i++)
+  {
+      pinMode(i, OUTPUT);
+      digitalWrite(i, HIGH);
+  }
+  int fa = [1,0,0,0,0,0,0,0];
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
